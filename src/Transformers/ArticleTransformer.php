cc48b27ff45a462a079c8dfb9acc7d4b2b7991d3
Konstantin Bukin <bukin@inetstudio.ru>
2017-12-22 01:55:52 +0300

    Add publish date to index

--- src/Transformers/ArticleTransformer.php
+++ src/Transformers/ArticleTransformer.php
@@ -5,13 +5,20 @@ namespace InetStudio\Articles\Transformers;
 use League\Fractal\TransformerAbstract;
 use InetStudio\Articles\Models\ArticleModel;
 
+/**
+ * Class ArticleTransformer
+ * @package InetStudio\Articles\Transformers
+ */
 class ArticleTransformer extends TransformerAbstract
 {
     /**
      * Подготовка данных для отображения в таблице.
      *
      * @param ArticleModel $article
+     *
      * @return array
+     *
+     * @throws \Throwable
      */
     public function transform(ArticleModel $article): array
     {
@@ -23,6 +30,7 @@ class ArticleTransformer extends TransformerAbstract
             ])->render(),
             'created_at' => (string) $article->created_at,
             'updated_at' => (string) $article->updated_at,
+            'publish_date' => (string) $article->publish_date,
             'actions' => view('admin.module.articles::back.partials.datatables.actions', [
                 'id' => $article->id,
                 'href' => $article->href,
