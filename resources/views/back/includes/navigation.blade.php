352a047b657c1ef6694bca7528a49b6b605dd5ec
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-09 19:52:12 +0300

    Fix icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.articles.*') }}">
-    <a href="{{ route('back.articles.index') }}"><i class="fa fa-file-text-o"></i> <span class="nav-label">Статьи</span></a>
+    <a href="{{ route('back.articles.index') }}"><i class="fa fa-file-alt"></i> <span class="nav-label">Статьи</span></a>
 </li>
