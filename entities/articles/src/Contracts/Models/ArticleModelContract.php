45c37402d099609e12be0a8949d8d6d768ad4d91
Konstantin Bukin <bukin@inetstudio.ru>
2020-01-30 19:09:39 +0300

    Remove favorites package

--- entities/articles/src/Contracts/Models/ArticleModelContract.php
+++ entities/articles/src/Contracts/Models/ArticleModelContract.php
@@ -6,11 +6,10 @@ use OwenIt\Auditing\Contracts\Auditable;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use InetStudio\Rating\Contracts\Models\Traits\RateableContract;
 use InetStudio\AdminPanel\Base\Contracts\Models\BaseModelContract;
-use InetStudio\Favorites\Contracts\Models\Traits\FavoritableContract;
 
 /**
  * Interface ArticleModelContract.
  */
-interface ArticleModelContract extends BaseModelContract, Auditable, FavoritableContract, HasMedia, RateableContract
+interface ArticleModelContract extends BaseModelContract, Auditable, HasMedia, RateableContract
 {
 }
