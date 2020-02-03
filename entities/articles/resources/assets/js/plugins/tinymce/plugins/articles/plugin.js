bd335d404a16a371f93f42291542a15855ec3a64
Konstantin Bukin <bukin@inetstudio.ru>
2020-02-03 17:46:59 +0300

    Fix widget load

--- entities/articles/resources/assets/js/plugins/tinymce/plugins/articles/plugin.js
+++ entities/articles/resources/assets/js/plugins/tinymce/plugins/articles/plugin.js
@@ -13,7 +13,7 @@ window.tinymce.PluginManager.add('articles', function(editor) {
     },
   };
 
-  function initFormsComponents() {
+  function initArticlesComponents() {
     if (typeof window.Admin.vue.modulesComponents.$refs['articles-package_ArticleWidget'] == 'undefined') {
       window.Admin.vue.modulesComponents.modules['articles-package'].components = _.union(
           window.Admin.vue.modulesComponents.modules['articles-package'].components, [
@@ -22,13 +22,15 @@ window.tinymce.PluginManager.add('articles', function(editor) {
               data: widgetData,
             },
           ]);
-    } else {
-      let component = window.Admin.vue.modulesComponents.$refs['articles-package_ArticleWidget'][0];
-
-      component.$data.model.id = widgetData.model.id;
     }
   }
-  
+
+  function loadWidget() {
+    let component = window.Admin.vue.modulesComponents.$refs['articles-package_ArticleWidget'][0];
+
+    component.$data.model.id = widgetData.model.id;
+  }
+
   editor.addButton('add_article_widget', {
     title: 'Статьи',
     icon: 'fa fa-file-alt',
@@ -43,9 +45,11 @@ window.tinymce.PluginManager.add('articles', function(editor) {
           id: parseInt($(content).attr('data-id')) || 0,
         };
 
-        initFormsComponents();
+        initArticlesComponents();
 
         window.waitForElement('#add_article_widget_modal', function() {
+          loadWidget();
+
           $('#add_article_widget_modal').modal();
         });
       } else {
